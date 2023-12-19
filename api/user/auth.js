1d15711c9a4570069ff13a780c67c71f07900e45
eleskin <eleskin1201@gmail.com>
2023-12-19 04:28:41 +0300

    Added token and logout routes

--- api/user/auth.js
+++ api/user/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const generateAccessToken = require('../../services/functions/generateAccessToken');
 const {ServerApiVersion, MongoClient} = require('mongodb');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -15,14 +14,16 @@ const client = new MongoClient(process.env.MONGODB_URL, {
 	},
 });
 
+let refreshTokens = [];
+
 router.post('/register', async (req, res) => {
-	const { email, password } = req.body;
+	const {email, password} = req.body;
 	
 	bcrypt.hash(password, saltRounds, async (err, hash) => {
 		const usersCollection = await client.db('main').collection('users');
-		const users = await usersCollection.find().toArray();
+		const users = await usersCollection.find({email}).toArray();
 		
-		const isExistUser = Boolean(users.find((user) => user.email === email));
+		const isExistUser = Boolean(users.length);
 		
 		if (isExistUser) {
 			res.status(400).send({
@@ -31,10 +32,78 @@ router.post('/register', async (req, res) => {
 		} else {
 			await usersCollection.insertOne({...req.body, password: hash});
 			
-			const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
-			res.status(201).send({accessToken});
+			const accessToken = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+			const refreshToken = jwt.sign({email}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1d'});
+			res.status(201).send({accessToken, refreshToken});
 		}
 	});
+});
+
+router.post('/login', async (req, res) => {
+	const {email, password} = req.body;
+	
+	const usersCollection = await client.db('main').collection('users');
+	const users = await usersCollection.find({email}).toArray();
+	
+	const isExistUser = Boolean(users.length);
+	
+	if (!isExistUser) {
+		res.status(400).send({
+			message: 'There is no user with this email address',
+		});
+	} else {
+		
+		bcrypt.compare(password, users[0].password, (err, result) => {
+			if (result) {
+				const accessToken = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+				const refreshToken = jwt.sign({email}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1d'});
+				
+				refreshTokens.push(refreshToken);
+				
+				res.status(200).send({
+					accessToken,
+					refreshToken,
+				});
+			} else {
+				res.status(400).send({
+					message: 'Invalid password',
+				});
+			}
+		});
+	}
+});
+
+router.post('/token', (req, res) => {
+	const {token} = req.body;
+	
+	if (!token) {
+		return res.sendStatus(401);
+	}
+	
+	if (!refreshTokens.includes(token)) {
+		return res.sendStatus(403);
+	}
+	
+	jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+		if (err) {
+			return res.sendStatus(403);
+		}
+		
+		const accessToken = jwt.sign(
+			{email: user.email},
+			process.env.ACCESS_TOKEN_SECRET,
+			{expiresIn: '1h'},
+		);
+		
+		res.status(200).send({accessToken});
+	});
+});
+
+router.post('/logout', (req, res) => {
+	const {token} = req.body;
+	refreshTokens = refreshTokens.filter((refreshToken) => refreshToken !== token);
+	
+	res(200).send('Logout successful');
 });
 
 module.exports = router;
--- services/functions/generateAccessToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateAccessToken = (email) => jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
-
-module.exports = generateAccessToken;
