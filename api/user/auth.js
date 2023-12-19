284608b6ecf06576409b1065e596e44b6d78ff52
eleskin <eleskin1201@gmail.com>
2023-12-19 03:41:09 +0300

    Added register route

--- api/user/auth.js
+++ api/user/auth.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const generateAccessToken = require('../../services/functions/generateAccessToken');
 const {ServerApiVersion, MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
+const saltRounds = 10;
 
 const client = new MongoClient(process.env.MONGODB_URL, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 router.post('/register', async (req, res) => {
-    const { email, password } = req.body;
-    
-    const users = JSON.stringify(await client.db('main').collection('messages').find().toArray());
-    
-    const user = users.find((user) => user.email === email && user.password === password);
-
-    const token = generateAccessToken(email);
-    res.send(token);
-
-    // res.send();
+	const { email, password } = req.body;
+	
+	bcrypt.hash(password, saltRounds, async (err, hash) => {
+		const usersCollection = await client.db('main').collection('users');
+		const users = await usersCollection.find().toArray();
+		
+		const isExistUser = Boolean(users.find((user) => user.email === email));
+		
+		if (isExistUser) {
+			res.status(400).send({
+				message: 'A user with this email address already exists',
+			});
+		} else {
+			await usersCollection.insertOne({...req.body, password: hash});
+			
+			const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
+			res.status(201).send({accessToken});
+		}
+	});
 });
 
 module.exports = router;
