1fdaa4c6b89a96b21f9d0d3b3a05cbe0306ed558
eleskin <eleskin1201@gmail.com>
2023-12-21 04:32:04 +0300

    Added chat switching

--- api/chat/messages.js
+++ api/chat/messages.js
@@ -3,6 +3,7 @@ const client = require('../../services/client');
 const http = require('http');
 const WebSocket = require('ws');
 const getMessages = require('../../services/functions/getMessages');
+const authenticateJWT = require('../../services/functions/authenticateJWT');
 
 const router = express.Router();
 const app = express();
@@ -36,5 +37,50 @@ wss.on('connection', async (ws) => {
 });
 
 server.listen(8000);
+
+router.post('/dialogs', authenticateJWT, async (req, res) => {
+	const {username} = req.body;
+	
+	const usersCollection = await client.db('main').collection('users');
+	const messagesCollection = await client.db('main').collection('messages');
+	const dialogsUsernames = (await messagesCollection
+		.find({$or: [{username}, {conversationalist: username}]})
+		.toArray())
+		.map((dialog) => [dialog.username, dialog.conversationalist]);
+	
+	const usernames = [...new Set(dialogsUsernames.flat())].filter((dialogUsername) => dialogUsername !== username);
+	
+	const dialogs = await usersCollection
+		.find({username: {$in: usernames}}, {projection: {name: 1, username: 1, _id: 0}})
+		.toArray();
+	
+	return res.status(200).send({
+		dialogs,
+	});
+})
+
+router.post('/chat', authenticateJWT, async (req, res) => {
+	const {username, conversationalist} = req.body;
+	
+	// const usersCollection = await client.db('main').collection('users');
+	const messagesCollection = await client.db('main').collection('messages');
+	const messages = await messagesCollection
+		.find({$or: [{username, conversationalist}, {username: conversationalist, conversationalist: username}]})
+		.toArray();
+	// const dialogsUsernames = (await messagesCollection
+	// 	.find({$or: [{username}, {conversationalist: username}]})
+	// 	.toArray())
+	// 	.map((dialog) => [dialog.username, dialog.conversationalist]);
+	//
+	// const usernames = [...new Set(dialogsUsernames.flat())].filter((dialogUsername) => dialogUsername !== username);
+	//
+	// const dialogs = await usersCollection
+	// 	.find({username: {$in: usernames}}, {projection: {name: 1, username: 1, _id: 0}})
+	// 	.toArray();
+	//
+	return res.status(200).send({
+		messages,
+	});
+})
 
 module.exports = router;
