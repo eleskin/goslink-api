855843fc856bb09626c66dbdf4871158ccc39327
eleskin <eleskin1201@gmail.com>
2023-12-22 02:58:55 +0300

    Added chat rooms

--- api/chat/messages.js
+++ api/chat/messages.js
@@ -13,26 +13,55 @@ const users = new Set();
 
 wss.on('connection', async (ws) => {
 	await client.connect();
-	users.add(ws);
+	// users.add(ws);
 	
 	ws.on('message', async (_data) => {
 		const data = JSON.parse(_data);
-		const messagesCollection = await client.db('main').collection('messages');
+		const users = [data.username, data.conversationalist].toSorted()
+		const roomName = users.join('|');
+		ws.roomName = roomName;
 		
+		const roomsCollection = await client.db('main').collection('rooms');
+		const usersCollection = await client.db('main').collection('users');
+		const messagesCollection = await client.db('main').collection('messages');
+
 		if (data.method === 'GET') {
 			console.log(data.method);
 		} else if (data.method === 'POST') {
-			await messagesCollection.insertOne({
-				username: data.username,
-				conversationalist: data.conversationalist,
-				message: data.message,
-				name: data.name,
-			});
+			const room = await roomsCollection.findOne({roomName});
+			
+			if (room) {
+				await messagesCollection.insertOne({
+					room: room._id,
+					text: data.message,
+					user: data.username,
+				});
+			} else {
+				const [firstUser, secondUser] = [
+					(await usersCollection.findOne({username: users[0]}))._id,
+					(await usersCollection.findOne({username: users[1]}))._id,
+				];
+				
+				const {insertedId} = await roomsCollection.insertOne({
+					firstUser,
+					secondUser,
+					roomName,
+				});
+				
+				await messagesCollection.insertOne({
+					room: insertedId,
+					text: data.message,
+					user: data.username,
+				});
+			}
 		}
 		
-		const messages = JSON.stringify(await getMessages(data.username, data.conversationalist, messagesCollection));
-		for(const user of users) {
-			user.send(messages);
+		for (const client1 of wss.clients) {
+			if (client1.roomName === ws.roomName) {
+				const {_id} = await roomsCollection.findOne({roomName: ws.roomName});
+				const messages = await messagesCollection.find({room: _id}).toArray();
+				client1.send(JSON.stringify(messages));
+			}
 		}
 	});
 });
@@ -40,24 +69,24 @@ wss.on('connection', async (ws) => {
 server.listen(8000);
 
 router.post('/dialogs', authenticateJWT, async (req, res) => {
-	const {username} = req.body;
-	
-	const usersCollection = await client.db('main').collection('users');
-	const messagesCollection = await client.db('main').collection('messages');
-	const dialogsUsernames = (await messagesCollection
-		.find({$or: [{username}, {conversationalist: username}]})
-		.toArray())
-		.map((dialog) => [dialog.username, dialog.conversationalist]);
-	
-	const usernames = [...new Set(dialogsUsernames.flat())].filter((dialogUsername) => dialogUsername !== username);
-	
-	const dialogs = await usersCollection
-		.find({username: {$in: usernames}}, {projection: {name: 1, username: 1, _id: 0}})
-		.toArray();
-	
-	return res.status(200).send({
-		dialogs,
-	});
+	// const {username} = req.body;
+	//
+	// const usersCollection = await client.db('main').collection('users');
+	// const messagesCollection = await client.db('main').collection('messages');
+	// const dialogsUsernames = (await messagesCollection
+	// 	.find({$or: [{username}, {conversationalist: username}]})
+	// 	.toArray())
+	// 	.map((dialog) => [dialog.username, dialog.conversationalist]);
+	//
+	// const usernames = [...new Set(dialogsUsernames.flat())].filter((dialogUsername) => dialogUsername !== username);
+	//
+	// const dialogs = await usersCollection
+	// 	.find({username: {$in: usernames}}, {projection: {name: 1, username: 1, _id: 0}})
+	// 	.toArray();
+	//
+	// return res.status(200).send({
+	// 	dialogs,
+	// });
 })
 
 module.exports = router;
