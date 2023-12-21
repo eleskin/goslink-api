7a5609dfd5066f2fe563c2831c81190bbe3633fe
eleskin <eleskin1201@gmail.com>
2023-12-21 04:32:23 +0300

    Added chat switching

--- api/chat/messages.js
+++ api/chat/messages.js
@@ -62,22 +62,11 @@ router.post('/dialogs', authenticateJWT, async (req, res) => {
 router.post('/chat', authenticateJWT, async (req, res) => {
 	const {username, conversationalist} = req.body;
 	
-	// const usersCollection = await client.db('main').collection('users');
 	const messagesCollection = await client.db('main').collection('messages');
 	const messages = await messagesCollection
 		.find({$or: [{username, conversationalist}, {username: conversationalist, conversationalist: username}]})
 		.toArray();
-	// const dialogsUsernames = (await messagesCollection
-	// 	.find({$or: [{username}, {conversationalist: username}]})
-	// 	.toArray())
-	// 	.map((dialog) => [dialog.username, dialog.conversationalist]);
-	//
-	// const usernames = [...new Set(dialogsUsernames.flat())].filter((dialogUsername) => dialogUsername !== username);
-	//
-	// const dialogs = await usersCollection
-	// 	.find({username: {$in: usernames}}, {projection: {name: 1, username: 1, _id: 0}})
-	// 	.toArray();
-	//
+	
 	return res.status(200).send({
 		messages,
 	});
