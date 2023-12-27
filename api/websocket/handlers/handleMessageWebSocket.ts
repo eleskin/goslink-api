0718c66455478bb23b572db4d1ef0847d5ab3466
eleskin <eleskin1201@gmail.com>
2023-12-28 01:56:46 +0300

    Added new websocket interface

--- api/websocket/handlers/handleMessageWebSocket.ts
+++ api/websocket/handlers/handleMessageWebSocket.ts
@@ -1,17 +1,88 @@
 import {webSocketResponseMessagesTypes} from '../types';
-import MessageData from '../interfaces/MessageData';
+import createNewUserResponse from '../services/createNewUserResponse';
+import createNewMessageResponse from '../services/createNewMessageResponse';
+import createDeleteMessageResponse from '../services/createDeleteMessageResponse';
+import WebSocket from 'ws';
+import {MongoClient} from 'mongodb';
 
 const messageHandlers: { [key in webSocketResponseMessagesTypes]: Function } = {
-	NEW_MESSAGE: () => {},
-	GET_MESSAGES: () => {},
-	EDIT_MESSAGE: () => {},
-	DELETE_MESSAGE: () => {},
+	NEW_MESSAGE: () => {
+	},
+	GET_MESSAGES: () => {
+	},
+	EDIT_MESSAGE: () => {
+	},
+	DELETE_MESSAGE: () => {
+	},
 };
 
-const handleMessageWebSocket = (_data: string) => {
-	const data: MessageData = JSON.parse(_data);
+const handleMessageWebSocket = async (
+	client: MongoClient,
+	wss: WebSocket.Server,
+	ws: WebSocket & { roomName: string },
+	_data: string,
+) => {
+	const data = JSON.parse(_data);
 	
+	const users = [data.username, data.conversationalist];
+	users.sort();
 	
-}
+	const roomName = users.join('|');
+	
+	const usersCollection = client.db('main').collection('users');
+	const roomsCollection = client.db('main').collection('rooms');
+	const messagesCollection = client.db('main').collection('messages');
+	
+	const userId = (await usersCollection.findOne({username: data.username}))?._id;
+	
+	if (!userId) return;
+	
+	ws.roomName = roomName;
+	
+	if (data.type === 'NEW_USER') {
+		await createNewUserResponse(
+			{usersCollection},
+			ws,
+			wss,
+			data,
+		);
+	} else if (data.type === 'NEW_MESSAGE') {
+		await createNewMessageResponse(
+			{roomsCollection, usersCollection, messagesCollection},
+			wss,
+			{roomName, userId, users, data},
+		);
+	} else if (data.type === 'DELETE_MESSAGE') {
+		await createDeleteMessageResponse(
+			{messagesCollection},
+			wss,
+			data,
+		);
+	}
+	
+	for (const client of Array.from(wss.clients)) {
+		if ((client as any).roomName !== ws.roomName) continue;
+		
+		const _id = (await roomsCollection.findOne({roomName: ws.roomName}))?._id;
+		
+		if (!_id) continue;
+		
+		const firstUserId = (await roomsCollection.findOne({roomName: ws.roomName}))?.firstUserId;
+		const secondUserId = (await roomsCollection.findOne({roomName: ws.roomName}))?.secondUserId;
+		
+		const messages = (await messagesCollection.find({roomId: _id}).toArray());
+		const conversationalistName = (await usersCollection.findOne({_id: userId.toString() !== firstUserId.toString() ? firstUserId : secondUserId}))?.name;
+		
+		for (const message of messages) {
+			message.author = (await usersCollection.findOne({_id: message.userId}))?.name;
+		}
+		
+		client.send(JSON.stringify({
+			type: 'NEW_MESSAGES',
+			conversationalistName,
+			messages,
+		}));
+	}
+};
 
 export default handleMessageWebSocket;
--- api/websocket/index.ts
+++ api/websocket/index.ts
@@ -1,13 +1,11 @@
-import express from 'express';
-import client from '../../services/client';
-import http from 'http';
 import WebSocket from 'ws';
-import createNewMessageResponse from './services/createNewMessageResponse';
-import createNewUserResponse from './services/createNewUserResponse';
-import createDeleteMessageResponse from './services/createDeleteMessageResponse';
-import getIdFromUrl from '../../services/functions/getIdFromUrl';
+import http from 'http';
+import express from 'express';
 import handleConnectionWebSocket from './handlers/handleConnectionWebSocket';
+import handleMessageWebSocket from './handlers/handleMessageWebSocket';
 import handleCloseWebSocket from './handlers/handleCloseWebSocket';
+import getIdFromUrl from '../../services/functions/getIdFromUrl';
+import client from '../../services/client';
 
 const app = express();
 const server = http.createServer(app).listen(8000);
@@ -15,78 +13,19 @@ const wss = new WebSocket.Server({server});
 
 const connectedClients: Set<string> = new Set();
 
-wss.on('connection', async (ws: any, request) => {
-	const _id = getIdFromUrl(request);
-	ws._id = _id;
-	
-	await client.connect();
-	await handleConnectionWebSocket(client, wss, ws, connectedClients);
-
-	ws.on('message', async (_data: any) => {
-		const data = JSON.parse(_data);
+wss.on(
+	'connection',
+	async (ws: WebSocket & { _id: string; roomName: string }, request,
+	) => {
+		const _id = getIdFromUrl(request);
+		ws._id = _id;
 		
-		const users = [data.username, data.conversationalist];
-		users.sort();
+		await client.connect();
+		await handleConnectionWebSocket(client, wss, ws, connectedClients);
 		
-		const roomName = users.join('|');
+		ws.on('message', (data: string) => handleMessageWebSocket(client, wss, ws, data));
 		
-		const usersCollection = client.db('main').collection('users');
-		const roomsCollection = client.db('main').collection('rooms');
-		const messagesCollection = client.db('main').collection('messages');
-		
-		const userId = (await usersCollection.findOne({username: data.username}))?._id;
-		
-		if (!userId) return;
-		
-		ws.roomName = roomName;
-		
-		if (data.type === 'NEW_USER') {
-			await createNewUserResponse(
-				{usersCollection},
-				ws,
-				wss,
-				data,
-			);
-		} else if (data.type === 'NEW_MESSAGE') {
-			await createNewMessageResponse(
-				{roomsCollection, usersCollection, messagesCollection},
-				wss,
-				{roomName, userId, users, data},
-			);
-		} else if (data.type === 'DELETE_MESSAGE') {
-			await createDeleteMessageResponse(
-				{messagesCollection},
-				wss,
-				data,
-			);
-		}
-		
-		for (const client of Array.from(wss.clients)) {
-			if ((client as any).roomName !== ws.roomName) continue;
-			
-			const _id = (await roomsCollection.findOne({roomName: ws.roomName}))?._id;
-			
-			if (!_id) continue;
-			
-			const firstUserId = (await roomsCollection.findOne({roomName: ws.roomName}))?.firstUserId;
-			const secondUserId = (await roomsCollection.findOne({roomName: ws.roomName}))?.secondUserId;
-			
-			const messages = (await messagesCollection.find({roomId: _id}).toArray());
-			const conversationalistName = (await usersCollection.findOne({_id: userId.toString() !== firstUserId.toString() ? firstUserId : secondUserId}))?.name;
-			
-			for (const message of messages) {
-				message.author = (await usersCollection.findOne({_id: message.userId}))?.name;
-			}
-			
-			client.send(JSON.stringify({
-				type: 'NEW_MESSAGES',
-				conversationalistName,
-				messages,
-			}));
-		}
+		ws.on('close', () => handleCloseWebSocket(connectedClients, _id));
 	});
-	
-	ws.on('close', () => handleCloseWebSocket(connectedClients, _id));
-});
 
 export default app;
