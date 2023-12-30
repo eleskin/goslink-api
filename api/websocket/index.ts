d8c5efbcd4eb0200243f402c14d3c245377e372a
eleskin <eleskin1201@gmail.com>
2023-12-30 14:47:24 +0300

    Refactoring

--- api/websocket/index.ts
+++ api/websocket/index.ts
@@ -6,26 +6,34 @@ import handleMessageWebSocket from './handlers/handleMessageWebSocket';
 import handleCloseWebSocket from './handlers/handleCloseWebSocket';
 import getIdFromUrl from '../../services/functions/getIdFromUrl';
 import client from '../../services/client';
+import app from './services/_websocket';
 
-const app = express();
-const server = http.createServer(app).listen(8000);
-const wss = new WebSocket.Server({server});
 
-const connectedClients: Set<string> = new Set();
-
-wss.on(
-	'connection',
-	async (ws: WebSocket & { _id: string; roomName: string }, request,
-	) => {
-		const _id = getIdFromUrl(request);
-		ws._id = _id;
-		
-		await client.connect();
-		await handleConnectionWebSocket(client, wss, ws, connectedClients);
-		
-		ws.on('message', (data: string) => handleMessageWebSocket(client, wss, ws, data));
-		
-		ws.on('close', () => handleCloseWebSocket(wss, connectedClients, _id));
-	});
+// const server = http.createServer(app).listen(8000);
+// const wss = new WebSocket.Server({server});
+//
+// const connectedClients: Set<string> = new Set();
+//
+// wss.on(
+// 	'connection',
+// 	async (ws: WebSocket & { _id: string; roomName: string }, request,
+// 	) => {
+// 		console.log(getIdFromUrl(request))
+// 		const _id = getIdFromUrl(request);
+// 		ws._id = _id;
+// 		console.log(_id);
+// 		console.log(ws._id);
+//
+// 		await client.connect();
+// 		console.log(ws);
+// 		const messagesCollection = client.db('name').collection('messages');
+// 		ws.send(JSON.stringify({type: 'GET_MESSAGES', data: {messages: messagesCollection.find({userId: _id}).toArray()}}))
+// 		// await handleConnectionWebSocket(client, wss, ws, connectedClients);
+//
+// 		ws.on('message', (data: string) => handleMessageWebSocket(client, wss, ws, data));
+//
+// 		ws.on('close', () => handleCloseWebSocket(wss, connectedClients, _id));
+//
+// 	});
 
 export default app;
