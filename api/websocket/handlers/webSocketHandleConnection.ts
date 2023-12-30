c952f14d04464a6aeb8c4110db349cbbb1aa8402
eleskin <eleskin1201@gmail.com>
2023-12-30 22:09:13 +0300

    Refactoring

--- api/websocket/handlers/webSocketHandleConnection.ts
+++ api/websocket/handlers/webSocketHandleConnection.ts
@@ -10,19 +10,31 @@ const webSocketHandleConnection = async (
 	ws: WebSocket & { _id: string; roomId: string },
 	request: http.IncomingMessage,
 ) => {
-	ws._id = getParamFromUrl(request, '_id');
-	ws.roomId = getParamFromUrl(request, 'room_id');
+	ws._id = getParamFromUrl(request, '_id') ?? '';
+	ws.roomId = getParamFromUrl(request, 'room_id') ?? '';
 	
-	const {roomsCollection} = await getCollections();
+	const {roomsCollection, usersCollection, messagesCollection} = await getCollections();
 	
 	const rooms = await roomsCollection.find({
 		$or: [{firstUserId: new ObjectId(ws._id)}, {secondUserId: new ObjectId(ws._id)}],
 	}).toArray();
 	
-	// ws.send(JSON.stringify({
-	// 	type: 'GET_MESSAGES',
-	// 	data: {rooms},
-	// }));
+	for (const room of rooms) {
+		const conversationalistId = room.firstUserId.toString() === ws._id.toString() ? room.secondUserId : room.firstUserId;
+		
+		room.conversationalist = conversationalistId;
+		room.conversationalistName = (await usersCollection.findOne({_id: conversationalistId}))?.name;
+		
+		const messages = messagesCollection.find({
+			roomId: room._id,
+		}).sort({_id: -1}).limit(1);
+		room.lastMessage = (await messages.toArray())[0]?.text;
+	}
+	
+	ws.send(JSON.stringify({
+		type: 'GET_ROOMS',
+		data: {rooms},
+	}));
 	
 	ws.on('message', (payload: string) => {
 		webSocketHandleMessage(ws, JSON.parse(payload));
