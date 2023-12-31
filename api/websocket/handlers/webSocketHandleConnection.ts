34dbacb206c30b39abeb3d918abac6960b1580ee
eleskin <eleskin1201@gmail.com>
2023-12-31 12:27:32 +0300

    Refactoring

--- api/websocket/handlers/webSocketHandleConnection.ts
+++ api/websocket/handlers/webSocketHandleConnection.ts
@@ -12,20 +12,32 @@ const webSocketHandleConnection = async (
 	wss: WebSocket.Server,
 	request: http.IncomingMessage,
 ) => {
-	ws._id = getParamFromUrl(request, '_id') ?? '';
+	const _id = getParamFromUrl(request, '_id') ?? '';
+	const contactId = getParamFromUrl(request, 'contact_id') ?? '';
+	
+	ws._id = _id;
 	
 	const {roomsCollection, usersCollection, messagesCollection} = await getCollections();
 	
-	const rooms = await roomsCollection.find({
-		$or: [{firstUserId: new ObjectId(ws._id)}, {secondUserId: new ObjectId(ws._id)}],
-	}).toArray();
+	const roomsFilter = contactId ?
+		{
+			$or: [
+				{$and: [{firstUserId: new ObjectId(_id)}, {secondUserId: new ObjectId(contactId)}]},
+				{$and: [{firstUserId: new ObjectId(contactId)}, {secondUserId: new ObjectId(_id)}]},
+			],
+		} :
+		{
+			$or: [{firstUserId: new ObjectId(_id)}, {secondUserId: new ObjectId(_id)}],
+		};
+	
+	const rooms = await roomsCollection.find(roomsFilter).toArray();
 	
 	for (const room of rooms) {
-		const conversationalistId = room.firstUserId.toString() === ws._id.toString() ? room.secondUserId : room.firstUserId;
-		
-		room.conversationalist = conversationalistId;
-		room.conversationalistName = (await usersCollection.findOne({_id: conversationalistId}))?.name;
-		
+		const contactId = room.firstUserId.toString() === _id.toString() ? room.secondUserId : room.firstUserId;
+
+		room.contactId = contactId;
+		room.contactName = (await usersCollection.findOne({_id: contactId}))?.name;
+
 		const messages = messagesCollection.find({
 			roomId: room._id,
 		}).sort({_id: -1}).limit(1);
@@ -39,11 +51,22 @@ const webSocketHandleConnection = async (
 			messages: await getMessage(ws),
 		},
 	}));
+	// console.log(getParamFromUrl(request, '_id') ?? '');
+	// console.log(getParamFromUrl(request, 'contact_id') ?? '');
+	// ws._id = getParamFromUrl(request, '_id') ?? '';
+	//
+	//
+	//
 	
-	ws.on('message', (payload: string) => {
-		webSocketHandleMessage(ws, wss, JSON.parse(payload));
-	});
-	ws.on('error', webSocketHandleError);
+	//
+	
+	//
+	
+	//
+	// ws.on('message', (payload: string) => {
+	// 	webSocketHandleMessage(ws, wss, JSON.parse(payload));
+	// });
+	// ws.on('error', webSocketHandleError);
 };
 
 export default webSocketHandleConnection;
