ea6db1ec0faa1a750cecad650c6dfba00eea1cff
eleskin <eleskin1201@gmail.com>
2024-01-01 07:52:59 +0300

    Refactoring

--- api/websocket/index.ts
+++ api/websocket/index.ts
@@ -3,23 +3,39 @@ import http from 'http';
 import WebSocket from 'ws';
 import handleMessageWebSocket from './handlers/handleMessageWebSocket';
 import {Payload} from './types';
+import getIdFromUrl from '../../services/functions/getIdFromUrl';
 
 const app = express();
 
 const server = http.createServer(app).listen(8000);
 const wss = new WebSocket.Server({server});
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws: WebSocket & { _id: string }, request) => {
+	ws._id = getIdFromUrl(request);
+	
 	ws.on('message', async (payload: Payload) => {
 		payload = JSON.parse(payload.toString());
 		const data = await handleMessageWebSocket(payload);
 		
 		if (!data) return;
 		
-		ws.send(JSON.stringify({
-			type: payload.type,
-			data,
-		}));
+		if (payload.data?.userId && payload.data?.contactId) {
+			for (const client of wss.clients) {
+				if ((client as any)._id === payload.data.userId || (client as any)._id === payload.data.contactId) {
+					client.send(JSON.stringify({
+						type: payload.type,
+						data,
+					}));
+				}
+			}
+		} else {
+			ws.send(JSON.stringify({
+				type: payload.type,
+				data,
+			}));
+		}
+		
+		
 	});
 });
 wss.on('close', () => {
--- services/functions/getIdFromUrl.ts
+++ services/functions/getIdFromUrl.ts
@@ -1,9 +1,9 @@
 import http from 'http';
 
-const getParamFromUrl = (request: http.IncomingMessage, param: string): string => {
+const getParamFromUrl = (request: http.IncomingMessage): string => {
 	const url = new URL(request.url ?? '', `ws://${request.headers.host}`);
 	
-	return url.searchParams.get(param) ?? '';
+	return url.searchParams.get('_id') ?? '';
 };
 
 export default getParamFromUrl;
