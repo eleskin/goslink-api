27dae67a2f6a14c4e6d7cfd1a8064c7f668d6385
eleskin <eleskin1201@gmail.com>
2024-01-10 20:29:16 +0300

    Add group chats

--- api/websocket/services/MessageService.ts
+++ api/websocket/services/MessageService.ts
@@ -72,34 +72,13 @@ class MessageService extends WebSocketService {
 	
 	private static async deleteMessage() {
 		const _id = this.payload?.data._id ?? '';
-		const userId = this.payload?.data.userId ?? '';
-		const contactId = this.payload?.data.contactId ?? '';
-		
-		const roomIds = [userId, contactId];
-		roomIds.sort();
-		const roomKey = `${roomIds[0]}_${roomIds[1]}`;
 		
 		const messagesCollection = await this.getCollection('messages');
 		
 		await messagesCollection.deleteOne({_id: new ObjectId(_id)});
 		
-		const messages = await messagesCollection.find().toArray();
-		
-		const sortedMessages = messages.reduce((acc: any, message) => {
-			const ids = [message.userId, message.contactId];
-			ids.sort();
-			const key = `${ids[0]}_${ids[1]}`;
-			
-			acc[key] = message;
-			
-			return acc;
-		}, {});
-		
 		return {
 			removedMessageId: _id,
-			lastMessage: sortedMessages[roomKey],
-			userId,
-			contactId,
 		};
 	}
 	
